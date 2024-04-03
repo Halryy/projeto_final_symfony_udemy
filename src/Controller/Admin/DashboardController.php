f897b0d9da5390ed9f20ccc706843b965743bd62
appixHalry <halryhenry2004@gmail.com>
2024-04-03 10:57:46 -0300

    - Adicionando template de administração
    - Instalando o Twig

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DashboardController extends AbstractController
+{
+    #[Route('/admin/dashboard', name: 'app_admin_dashboard')]
+    public function index(): Response
+    {
+        return $this->render('admin/dashboard/index.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
+    }
+}
