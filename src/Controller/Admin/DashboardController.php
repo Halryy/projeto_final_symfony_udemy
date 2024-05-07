987fc1b07534589b4aac82f79bec8d40d3428f42
Halryy <halryhenry2004@gmail.com>
2024-05-07 11:08:31 -0300

    Novos features no dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\BannerRepository;
+use App\Repository\NewsRepository;
+use App\Repository\ProductRepository;
+use App\Repository\TestimonyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +13,23 @@ use Symfony\Component\Routing\Attribute\Route;
 class DashboardController extends AbstractController
 {
     #[Route('/admin/dashboard', name: 'app_admin_dashboard')]
-    public function index(): Response
+    public function index(
+        BannerRepository $bannerRepository,
+        NewsRepository $newsRepository,
+        ProductRepository $productRepository,
+        TestimonyRepository $testimonyRepository
+    ): Response
     {
+        $totalBanners = sizeof($bannerRepository->findAll());
+        $totalNews = sizeof($newsRepository->findAll());
+        $totalProducts = sizeof($productRepository->findAll());
+        $totalTestimony = sizeof($testimonyRepository->findAll());
         return $this->render('admin/dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+            'totalBanners' => $totalBanners,
+            'totalNews' => $totalNews,
+            'totalProducts' => $totalProducts,
+            'totalTestimony' => $totalTestimony,
+            'allNews' => $newsRepository->findBy([], ['date' => 'Desc']),
         ]);
     }
 }
